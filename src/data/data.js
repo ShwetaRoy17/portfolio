173830891e518dd0a526a8b84418c82c75ee7f6f
ShwetaRoy17 <royshweta1712@gmail.com>
2026-03-29 01:29:29 +0530

    code flow for data, main and app files

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const profile = {
+  name: "Shweta Roy",
+  title: "Software Developer",
+  github: "https://github.com/ShwetaRoy17",
+  linkedin: "https://www.linkedin.com/in/shweta-roy-104145227/",
+  leetcode: "https://leetcode.com/u/shwetaroy1712/",
+  email: "royshweta1712@gmail.com",
+  resumeView: "/resume/RoyShweta2026.pdf",
+  resumeDownload: "/resume/RoyShweta2026.pdf",
+};
